6ea695cd581b982dc5328905fbae31584f9e25a0
Igor Gonalves <igordossantos38@gmail.com>
2021-06-21 15:53:48 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function verificar() {
+    var data = c = new Date();
+    var ano = data.getFullYear();
+    var formAno = document.querySelector('input#txtano');
+    var res = document.querySelector('div#result');
+    
+}
