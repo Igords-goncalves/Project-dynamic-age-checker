10b674498cd53e5927215826c7aab823d7218c13
Igor Gonalves <igordossantos38@gmail.com>
2021-07-13 22:10:03 -0300

    Limpeza dos intens Js e Organização do código CSS

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 function verificar() {
-    var data = c = new Date();
+    var data = new Date();
     var ano = data.getFullYear();
     var formAno = document.querySelector('input#txtano');
     var res = document.querySelector('div#result');
+
     //Condition ========================================
     if (formAno.value.length == 0 || Number(formAno.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente');
     } else {
         var formSex = document.getElementsByName('radsex');
-        var idade = ano - Number(formAno.value);
+        var idade = ano - Number(formAno.value); //Conversão de entrada string p/ numero
         var genero = '';
-        var img = document.createElement('img');
+        var img = document.createElement('img'); //Estou criando aqui ao inves de fazer isso no 
         img.setAttribute('id','foto');
+
         //Man ==========================================
-        if (formSex[0].checked) {
+        if (formSex[0].checked) { //Por padrão o elemento radio é representado por 0 e 1
             genero = 'Homem';
             if (idade >= 0 && idade < 3) {
                 //Bebe
@@ -35,6 +37,7 @@ function verificar() {
                 img.setAttribute('src', './img/oldman.jpg');
             }
         }
+
         //Woman ========================================== 
         else 
             if (formSex[1].checked) {
@@ -59,6 +62,8 @@ function verificar() {
                     img.setAttribute('src', './img/oldwoman.jpg')
                 }
             }
+
+            //Resposta ao resultado obtido, usando um innerHTML - mostra na tela
             res.innerHTML = `Genero ${genero} com ${idade} anos`;
             res.appendChild(img);
     }
